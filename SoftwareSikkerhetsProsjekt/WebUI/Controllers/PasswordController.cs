f76d344169f167e8bbd62de32ee9aa14ca19f346
Yunus Yigit <yunusy@uia.no>
2023-11-06 21:25:06 +0100

    Add .gitignore file

---
+++ SoftwareSikkerhetsProsjekt/WebUI/Controllers/PasswordController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebUI.Controllers;
+
+public class PasswordController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
